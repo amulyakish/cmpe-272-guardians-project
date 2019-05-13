f8bf5e5b8bb6da99648b7f646b2b5b346b46a9b0
amuk <amuk@cisco.com>
2019-05-12 21:50:05 -0700

    UI Code committed

--- src/main/java/com/guardians/cmpe272/assignment/AssignmentApplication.java
+++ src/main/java/com/guardians/cmpe272/assignment/AssignmentApplication.java
@@ -2,10 +2,19 @@ package com.guardians.cmpe272.assignment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+import com.sun.glass.ui.Application;
 
 @SpringBootApplication
-public class AssignmentApplication {
+public class AssignmentApplication extends SpringBootServletInitializer{
 
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(AssignmentApplication.class, args);
 	}
