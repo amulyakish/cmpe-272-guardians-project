207e6f14975dd8ca5dd25751dbafd7d1f92a264b
amuk <amuk@cisco.com>
2019-05-19 20:33:58 -0700

    Added env variable

--- src/main/java/com/guardians/cmpe272/assignment/security/ResourceServerSecurityConfig.java
+++ src/main/java/com/guardians/cmpe272/assignment/security/ResourceServerSecurityConfig.java
@@ -1,7 +1,9 @@
 package com.guardians.cmpe272.assignment.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
@@ -11,8 +13,11 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Res
 @Configuration
 @EnableResourceServer
 public class ResourceServerSecurityConfig extends ResourceServerConfigurerAdapter {
-   @Value("${security.oauth2.resource.id}")
-   private String resourceId;
+   //@Value("http://walk/api")
+   //private String resourceId;
+   
+   @Autowired
+   private Environment env;
 
    @Override
    public void configure(HttpSecurity http) throws Exception {
@@ -26,6 +31,6 @@ public class ResourceServerSecurityConfig extends ResourceServerConfigurerAdapte
 
    @Override
    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
-       resources.resourceId(resourceId);
+       resources.resourceId(env.getProperty("security.oauth2.resource.id"));
    }
 }
